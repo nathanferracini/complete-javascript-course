ed753324891e2db9d6c8f13863630a2837730f0a
Nathan Ferracini <nathanferracini@gmail.com>
2021-11-23 18:47:33 -0300

    Add Developer Skills & Editor Setup codingChallenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 12;
+
+console.log(122, x);
+const answer = prompt("what is next?");
+
+console.log(answer, 12);
