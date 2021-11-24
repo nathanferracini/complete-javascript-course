adf072edcac1eea6bb1c01d74e97d269fc44ceed
Nathan Ferracini <nathanferracini@gmail.com>
2021-11-24 18:09:08 -0300

    Add Section 9 - coding challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +25,92 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const threeParametersFuncition = function ({
+  first,
+  second = 'second',
+  third,
+}) {
+  console.log('first', first);
+  console.log('second', second);
+  console.log('third', third);
+};
+
+threeParametersFuncition({
+  third: 'terceiro',
+  first: 'primeiro',
+});
+
+const {
+  name: restaurantName,
+  openingHours: {
+    fri: { open, close },
+  },
+  categories: tags,
+  nonExisting: priceRange = '$$$',
+} = restaurant;
+console.log(restaurantName, 'fri', tags, priceRange, open, close);
+
+// const {
+//   fri: { open, close },
+// } = hours;
+// console.log(open, close);
+
+// dd
+let [x, y, z] = [1, 2];
+console.log(x, y, z);
+
+let [first, second, , third] = restaurant.categories;
+console.log(first, second, third);
+
+[second, first, , third] = [first, second, , third];
+console.log(first, second, third);
+
+const [starter, mainCourse] = restaurant.order(0, 1);
+console.log(starter, mainCourse);
+
+const nested = [1, 2, 3, [4, 5]];
+const [i, , , [j, k]] = nested;
+console.log(i, j, k);
+
+const [p, q, r = 'default'] = [8, 9];
+console.log(p, q, r);
+
+const newArr = ['Spread', ...nested];
+console.log('whithout spread', newArr);
+console.log('whith spread', ...newArr);
+
+const originalObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+const objectSpread = { ...originalObject, d: 4, e: 5 };
+console.log('objectSpread', objectSpread);
+
+const { a: head, e: tail, ...objectRest } = objectSpread;
+console.log('objectRest', head, tail, objectRest);
+
+const [a, b, ...others] = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a, b, others);
+
+const rest1 = {
+  name: 'Capri',
+  numGuest: 20,
+};
+
+const rest2 = {
+  name: 'La Pizza',
+  owner: 'Pablo Palmares',
+};
+
+rest2.numGuest = rest2.numGuest || 10;
+console.log(rest2);
