25130ed92568a61d3732e65cac3b2139adde6ce8
Nathan Ferracini <nathanferracini@gmail.com>
2021-12-11 14:24:19 -0300

    Add if clause to handle errors

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -29,9 +29,12 @@ const renderSpinner = function (parent) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     renderSpinner(recipeContainer);
+
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -154,5 +157,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
-console.log('asAs');
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
